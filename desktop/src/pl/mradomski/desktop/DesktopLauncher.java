7b32858dc9688e133ff2feff0723380d0096b0b5
Marcin Radomski <marcin@mradomski.pl>
2016-01-30 18:47:00 +0100

    Initial commit

---
+++ desktop/src/pl/mradomski/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.mradomski.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.mradomski.FluidApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FluidApp(), config);
+	}
+}
